0b4e9e6c9c55f47b489c657e4745980bfc02dda8
Evgeny BesPredel63 <buyakov@hotmail.com>
2023-10-20 14:17:48 +0400

    Добавлен файл со списком url адресов

---
+++ src/url/urlApi.js
@@ -0,0 +1,7 @@
+export const SERVER_URL = 'http://localhost:5000/'
+
+export const CategoryGetAll = 'api/Categories'
+export const CategoryGetById = 'api/Categories/'
+
+export const GoalsGetAll = 'api/Goals'
+export const GoalsGetById = 'api/Goals/'
